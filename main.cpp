0015dd8bc9579809d688767f60856f66ac9eb466
lwalsh6 <lwalsh6@ut.utm.edu>
2022-08-25 12:30:22 -0500

    Test File

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @author Logan Walsh
+ * @date 2022-08-25
+ * @brief Git test 
+ * 
+ * this is a test program for github
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World" << endl;
+  
+  return 0;
+}
