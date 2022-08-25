d8a82899a3caed3af2cfef53e3d3aa1cec4d3a97
lwalsh6 <lwalsh6@ut.utm.edu>
2022-08-25 14:54:21 -0500

    Different Iteration

--- main.cpp
+++ main.cpp
@@ -12,8 +12,39 @@
 
 using namespace std;
 
+
+/**
+ * Used to add up all of the numbers from 1 to a given limit
+ *
+ * @param int n the limit to the values
+ * @return int Returns the sum of all the numbers
+ * @post The numbers have all been added
+ * 
+ */
+int sum(int n);
+
 int main() {
-  cout << "Hello World" << endl;
-  
+  int n;
+  int result;
+  //Opening statement 
+  cout << "Give me a number: ";
+  //reads in a number
+  cin >> n;
+  //calls function and retrieves result variable
+  result = sum(n);
+  //Prints out variable
+  cout << result << endl;
   return 0;
+}
+
+int sum(int n){
+  int i = 0;
+  //Used to retain previous i values that have already been added up
+  int i2 = 0;
+  for(i=0; i<=n; i++){
+    //Adds up previous value by the new one
+    i2 = i2+i;
+  }
+  //returns the full value
+  return i2;
 }
